0ce533227c856679c89a979bec168d9698e39bf2
Bruno Rocha <mail.brunorocha@gmai.com>
2017-11-29 22:58:49 -0300

    Adicionado efeito de scroll na página ao clicar no link do menu

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
+$(function() {
+    $("#header-nav li a").click(function(e){
+        var cur = $(this);
+
+        $('html, body').animate({
+          scrollTop: $("."+cur.attr("id")+"").offset().top - 120
+        }, 1000);
+    });
+});
+
+$(window).scroll(function(){
+    if($(this).scrollTop() > 550 && $(this).scrollTop() < 700) {
+        $(".active").removeClass("active");
+        $("#missao").addClass("active");
+    } else if($(this).scrollTop() > 700 && $(this).scrollTop() < 900) {
+        $(".active").removeClass("active");
+        $("#equipe").addClass("active");
+    } else if($(this).scrollTop() > 900) {
+        $(".active").removeClass("active");
+        $("#contato").addClass("active");
+    } else {
+        $(".active").removeClass("active");
+    }
+});
+
 function parallax() {
     var planetBlue = document.getElementById('planet-blue');
     var planetRed  = document.getElementById('planet-red');
